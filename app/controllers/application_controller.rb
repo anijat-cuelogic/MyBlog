56805eb857bb9ac268d9efddabc36f4565704202
ajuj-cuelogic <aju.john@cuelogic.co.in>
2015-10-15 19:03:20 +0530

    show warning msg if nonadmin user access rails admin view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :authenticate_user!
+  rescue_from CanCan::AccessDenied do |exception|
+    flash.now[:notice] = "Access denied!"
+    redirect_to main_app.root_path, :alert => exception.message
+  end
+  check_authorization unless: :devise_controller?
 
 end
