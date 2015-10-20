e4c3a842779742339e70bde60a97b117b8513596
Anija Thomas <anija.thomas@cuelogic.co.in>
2015-10-20 13:02:01 +0530

    Minitests for Post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'presence of post title and description' do
+    @post = FactoryGirl.create(:post)
+    assert_equal @post.valid?, true, 'valid post'
+  end
+
+  test 'absence of post params' do
+    @post = Post.new(title: '', description: '', author_id: nil)
+    assert_equal @post.valid?, false, 'post is invalid as title is blank'
+  end
+
+  test "should not save post without title and description" do
+    post = Post.new
+    assert_not post.save, "Saved the post with blank title & description"
+  end
 end
