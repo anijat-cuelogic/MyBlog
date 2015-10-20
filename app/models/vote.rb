90d777ad0d6101d97631fd070a1909f196c7b1b9
Anija Thomas <anija.thomas@cuelogic.co.in>
2015-10-20 19:15:51 +0530

    adding scope to count votes Up n Down

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :comment
+
+  scope :votes_up, -> (comment_id)  {where(comment_id: comment_id, value: true).count}
+  scope :votes_down, -> (comment_id)  {where(comment_id: comment_id, value: false).count}
 end
